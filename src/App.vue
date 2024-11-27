9455eb8b4a97e47f00cbb98d9221e9f817736669
Hammachi-Dalil <dalil.hammachi@edu.univ-fcomte.fr>
2024-11-27 14:45:11 +0100

    feat(header) : ajout du composant header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import MyHeader from './components/Header/Header.vue';
 
 </script>
 
 <template>
-
+  <MyHeader />
   <RouterView />
 </template>
 
