be107829ea661a408fce1bc582f5569da8a283ac
Hammachi-Dalil <dalil.hammachi@edu.univ-fcomte.fr>
2024-12-02 16:13:51 +0100

    feat(home) : import du composant home.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+
+import Home from '~/components/Home.vue';
 </script>
 
 <template>
     <div>
         <h1>Index</h1>
         <Button variant="primary">Primary</Button>
+        <Home variant="inscription"/>
        
     </div>
 
