65e18b47f888a18d8ed819cd37c15bf8f5148166
Hammachi-Dalil <dalil.hammachi@edu.univ-fcomte.fr>
2024-12-02 13:50:03 +0100

    feat(layout) : ajout de layout

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
-  <Header />
-  <NuxtPage  />
+  <NuxtLayout>
+    <NuxtPage  />
+  </NuxtLayout>
 </template>
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+
+definePageMeta({
+    layout: 'minimal'
+})
 </script>
 
 <template>
