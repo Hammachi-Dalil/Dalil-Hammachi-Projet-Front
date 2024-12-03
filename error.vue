b505836135dcf06633a9a0fcd5168322698f51ad
Hammachi-Dalil <dalil.hammachi@edu.univ-fcomte.fr>
2024-12-03 14:57:21 +0100

    feat(page error): ajout d'une page error

---
+++ error.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type {NuxtError} from '#app';
+
+defineProps({
+    error: {
+        type: Object as () => NuxtError,
+        default: null
+    }
+})
+
+const handleError = () => {
+    clearError({redirect: '/'});
+}
+</script>
+
+<template>  
+    <main>
+        <div v-if="error">
+            <H1>Template d'erreur personnalisé</H1>
+            <h2>Erreur {{ error.statusCode }}</h2>
+            <br>
+            <p>{{ error.message }}</p>
+            <br>
+            <Button @click="handleError">Retour à l'accueil</Button>
+        </div>
+        
+    </main>
+</template>
+
+<style>
+</style>
