4a02cc71768ffe6f82efbb975285058db39ea4ae
Hammachi-Dalil <dalil.hammachi@edu.univ-fcomte.fr>
2024-12-03 13:40:47 +0100

    feat(sanityContent): utilisation de sanity content pour afficher le contenu et l'image de manière propre.

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+import type { SanityDocument } from "@sanity/client";
+const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
+const route = useRoute();
+
+const {data: post} = await useSanityQuery<SanityDocument>(POST_QUERY, {slug: route.params.slug});
+
+console.log(post);
+
+
+</script>
+
+<template>
+  <main>
+    <div v-if="post">
+        Nom du slug : {{ route.params.slug }}
+        <h1>{{ post.title }}</h1>
+
+    
+        <SanityContent :blocks="post.body" />
+        <SanityImage :asset-id="post.image.asset._ref" alt="l'image"/>
+
+    
+    </div>
+  </main>
+</template>
---
+++ pages/blog/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { SanityDocument } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+
+const POSTS_QUERY = groq`*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, title, image, slug, publishedAt}`;
+
+const { data: posts } = await useSanityQuery<SanityDocument[]>(POSTS_QUERY);
+
+const { projectId, dataset } = useSanity().client.config();
+const urlFor = (source: SanityImageSource) =>
+  projectId && dataset
+    ? imageUrlBuilder({ projectId, dataset }).image(source)
+    : null;
+
+definePageMeta({
+    layout: 'minimal'
+})
+</script>
+
+<template>
+    <main>
+        <div>
+            <h1>Blog</h1>
+                <ul>
+                    <li v-for="post in posts" :key="post._id">
+                        <NuxtLink :to="`/blog/${post.slug.current}`">
+                            <p>{{ post.title }}</p>
+                            <img v-if="post.image" :src="urlFor(post.image)?.url()" alt="" >
+                        </NuxtLink>
+                        
+                    </li>
+                </ul>
+        </div>
+    </main>
+    
+</template> 
+
+<style lang = "scss">
+</style>
